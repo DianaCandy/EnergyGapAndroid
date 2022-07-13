d13f4e13a9b731c23994c9a6151cd8293790044f
Diana Candy <shiftregister233@outlook.com>
2022-07-13 23:43:03 +0800

    Android Init

---
+++ app/src/test/java/icu/takemeto/android/energygap/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package icu.takemeto.android.energygap
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
